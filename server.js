f0788083272b00dbd04ee4cb96549822ad2173b3
Tw. Shim <marocchino@gmail.com>
2011-11-29 21:50:18 +0900

    change name for dotcloud

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('coffee-script');
 var server = require('./notepad');
-var port = process.env.PORT || "3000";
+var port = process.env.PORT || "8080";
 if (!module.parent) {
   server.listen(port);
   console.log("Express server listening on port %d in %s mode", server.address().port, server.settings.env);
